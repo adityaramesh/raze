ba11f105c28d4758106e10ef83b12b02cc826fe1
Aditya Ramesh <_@adityaramesh.com>
2014-09-06 14:07:28 -0400

    Updated Id creation syntax.

--- docs/id_creation.cpp
+++ docs/id_creation.cpp
@@ -5,41 +5,14 @@
 ** Contact:	_@adityaramesh.com
 */
 
-#include <cstdint>
-#include <iostream>
-#include <typeinfo>
+template <class Char, class... Chars>
+struct basic_string {};
 
-struct string
-{
-	const uintmax_t m_str[64];
-	const size_t m_length;
-
-	template <class... Ts>
-	constexpr string(const Ts... ts) :
-	m_str{(uintmax_t)ts...}, m_length{sizeof...(Ts)} {}
-
-	constexpr size_t size() const { return m_length; }
-	constexpr size_t length() const { return m_length; }
-	constexpr uintmax_t operator[](size_t n) const { return m_str[n]; }
-};
-
-template <uintmax_t... Ts> struct sequence {};
-
-constexpr auto
-operator"" _id(const char* str, size_t n)
-{
-	return n == 0 ? string{} :
-	       n == 1 ? string{str[0]} :
-	       n == 2 ? string{str[0], str[1]} :
-	       n == 3 ? string{str[0], str[1], str[2]} :
-	       n == 4 ? string{str[0], str[1], str[2], str[3]} :
-	       n == 5 ? string{str[0], str[1], str[2], str[3], str[4]} :
-	                string{str[0], str[1], str[2], str[3], str[4], str[5]};
-}
+template <class Char, Char... Chars>
+constexpr basic_string<Char, Char...> operator"" _id()
+{ return basic_string<Char, Chars...>{}; }
 
 int main()
 {
-	static constexpr auto string = "Hello!"_id;
-	using sequence = sequence<string[0], string[1], string[2], string[3], string[4], string[5]>;
-	std::cout << typeid(sequence{}).name() << std::endl;
+
 }
